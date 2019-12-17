73e6cd7b7069490c9b56f54c02f817aabea7e436
Lois Talagrand <lois.talagrand@gmail.com>
2019-12-16 16:48:57 -1000

    ActionCable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Allow ActionCable requests 
+  # config.action_cable.allowed_request_origins = ["https://messagr.herokuapp.com"]
 end
