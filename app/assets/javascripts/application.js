24c112b6b9d988a36c52dba404469e3626b9bd92
Lois Talagrand <lois.talagrand@gmail.com>
2019-12-17 09:02:03 -1000

    Display chatroom participants

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,7 +29,6 @@ scroll_bottom = function() {
 submit_message = function() {
 	$("#message_body").on("keydown", function(e){
 		if (e.keyCode == 13) {
-			alert("13");
 			$("#writeMessageBox > button").click();
 			e.target.value = "";
 		};
